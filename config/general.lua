824c314e46f9bb21a03f05f9fcd22a0a2af23aa1
crcrworks <crcr.exe@gmail.com>
2023-12-31 18:13:06 +0900

    deleted unnecessary comment

--- config/general.lua
+++ config/general.lua
@@ -1,8 +1,8 @@
 local wezterm = require("wezterm")
 
 return {
-  -- use_ime = true,
-  -- adjust_window_size_when_changing_font_size = true,
-  allow_square_glyphs_to_overflow_width = "Never",
-  window_close_confirmation = "NeverPrompt",
+	-- use_ime = true,
+	-- adjust_window_size_when_changing_font_size = true,
+	allow_square_glyphs_to_overflow_width = "Never",
+	window_close_confirmation = "NeverPrompt",
 }
