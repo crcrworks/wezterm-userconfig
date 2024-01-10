f30aaf52fb46a82c59e8912eedce9a05e801470f
crcrworks <crcr.exe@gmail.com>
2024-01-11 02:11:53 +0900

    changed font config

--- config/fonts.lua
+++ config/fonts.lua
@@ -3,8 +3,12 @@ local wezterm = require("wezterm")
 return {
   font = wezterm.font_with_fallback({
     -- { family = "Zed Mono", assume_emoji_presentation = false },
-    { family = "Hack Nerd Font Mono", assume_emoji_presentation = true },
+    {
+      family = "Hack Nerd Font Mono",
+      assume_emoji_presentation = true,
+    },
     -- { family = "Symbols Nerd Font Mono" }
   }),
   font_size = 20,
+  harfbuzz_features = { "calt=0", "clig=1", "liga=0" },
 }
