1adaaf061e9145a482adc490122e360bf2bbce53
CRCR <crcr.exe@gmail.com>
2024-01-28 14:57:35 +0900

    文字が繋がるやつ、ONにした。

--- config/fonts.lua
+++ config/fonts.lua
@@ -5,10 +5,10 @@ return {
 		-- { family = "Zed Mono", assume_emoji_presentation = false },
 		{
 			family = "JetBrains Mono",
-			assume_emoji_presentation = true,
+			-- assume_emoji_presentation = true,
 		},
 		-- { family = "Symbols Nerd Font Mono" }
 	}),
 	font_size = 20,
-	harfbuzz_features = { "calt=0", "clig=1", "liga=0" },
+	-- harfbuzz_features = { "calt=0", "clig=1", "liga=0" },
 }
